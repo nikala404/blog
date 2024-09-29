8f70ebbd9de7bfc4e1cdfe92292502445b19985a
nikala404 <nikoloz.lursmanashvili.1@btu.edu.ge>
2024-09-29 14:24:11 +0400

    unmodified

--- src/main/java/com/example/blog/model/Post.java
+++ src/main/java/com/example/blog/model/Post.java
@@ -16,7 +16,9 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String text;
+
     @Column(name = "userName")
     private String userName;
 }

717a6d0ecce955448a461440cb9bdf2c870ea256
nikala404 <nikoloz.lursmanashvili.1@btu.edu.ge>
2024-09-30 11:25:29 +0400

    added fetcher and mapper methods tu fetch users from ReqRes api and set its email as my users username

--- src/main/java/com/example/blog/service/UserService.java
+++ src/main/java/com/example/blog/service/UserService.java
@@ -1,23 +1,54 @@
 package com.example.blog.service;
 
-import com.example.blog.model.Post;
+import com.example.blog.client.ReqResClient;
+import com.example.blog.dto.ReqResUser;
+import com.example.blog.dto.UserResponse;
 import com.example.blog.model.User;
-import com.example.blog.repository.PostRepository;
 import com.example.blog.repository.UserRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService  {
 
     private final  UserRepository userRepository;
+    private final ReqResClient reqResClient;
 
 
-    public UserService(UserRepository userRepository) {
+
+    public UserService(UserRepository userRepository, ReqResClient reqResClient) {
         this.userRepository = userRepository;
+        this.reqResClient = reqResClient;
     }
+
 
    public void registerUser(User user){
             userRepository.save(user);
    }
+
+    public void fetchAndStoreUsers() {
+        int page = 1;
+        UserResponse response;
+        do {
+            response = reqResClient.getAllUsers(page);
+            if (response != null && !response.getData().isEmpty()) {
+                List<User> users = response.getData().stream()
+                        .map(this::mapToUser)
+                        .toList();
+                userRepository.saveAll(users);
+                page++;
+            }
+        } while (response != null && !response.getData().isEmpty());
+    }
+
+    private User mapToUser(ReqResUser reqResUser) {
+        User user = new User();
+        user.setFirstName(reqResUser.getFirst_name());
+        user.setLastName(reqResUser.getLast_name());
+        user.setUserName(reqResUser.getEmail());
+        return user;
+    }
 
 }

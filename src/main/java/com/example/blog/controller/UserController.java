b80760cb1e0f3bf71530528fd49a035d42ec376e
nikala404 <nikoloz.lursmanashvili.1@btu.edu.ge>
2024-09-30 11:41:37 +0400

    user controller with registration endpoint

---
+++ src/main/java/com/example/blog/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.example.blog.controller;
+
+import com.example.blog.client.ReqResClient;
+import com.example.blog.model.User;
+import com.example.blog.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> createUser(@RequestBody User user){
+        try {
+            userService.registerUser(user);
+            return ResponseEntity.ok("User registered successfully");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+
+
+
+}

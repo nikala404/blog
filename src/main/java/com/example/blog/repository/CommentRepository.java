03694db5bbbaec3200ae4bf6e6201a465d07aa4b
nikala404 <nikoloz.lursmanashvili.1@btu.edu.ge>
2024-09-30 15:51:58 +0400

    added findByPostId to find posts with id easily

--- src/main/java/com/example/blog/repository/CommentRepository.java
+++ src/main/java/com/example/blog/repository/CommentRepository.java
@@ -1,9 +1,13 @@
 package com.example.blog.repository;
 
 import com.example.blog.model.Comment;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPostId(long postId);
 }
--- src/main/java/com/example/blog/repository/UserRepository.java
+++ src/main/java/com/example/blog/repository/UserRepository.java
@@ -2,11 +2,18 @@ package com.example.blog.repository;
 
 import com.example.blog.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 
-    boolean findByUserName (String username);
+    @Query("SELECT COUNT(u) > 0 FROM User u WHERE u.userName = :userName")
+    boolean existsByUserName(@Param("userName") String userName);
+
+    Optional<User> findUserByUserName(@Param("userName") String userName);
 }
 
